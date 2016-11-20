7a69163d4e81bb9d99727bb71906415fd71e9de3
Vlad160 <vlaner160@gmail.com>
2016-11-20 21:12:23 +0300

    добавил мой хуй

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample1.fxml"));
-        primaryStage.setTitle("Есть хуй");
+        primaryStage.setTitle("Есть мой хуй");
         primaryStage.setScene(new Scene(root, 650, 500));
         primaryStage.show();
     }
