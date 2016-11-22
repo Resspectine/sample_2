50abd814a5d93b0d8b464091c9fd4330ea560e29
Vlad160 <vlaner160@gmail.com>
2016-11-22 13:05:58 +0300

    Чат работает, нормально(годно)

---
+++ src/chat/Message.java
@@ -0,0 +1,14 @@
+package chat;
+
+public class Message {
+    private String message;
+
+    public Message() {
+        this.message = "";
+    }
+
+    public Message(String message) {
+        this.message = message;
+        message = message.concat("\n");
+    }
+}
