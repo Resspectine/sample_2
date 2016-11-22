143aa7fc82a4e1c70227ccfe4f4b8b26bf8d0501
Resspectine <resspectyn@gmail.com>
2016-11-22 14:05:02 +0300

    Рабочий чат и новый интерефейс

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,27 +1,28 @@
 package sample;
 
-import chat.*;
+import chat.Chat;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
 
 public class Controller {
-    @FXML
     public Button button;
-    @FXML
     public Button buttonBack;
-    @FXML
-    private TextArea chat;
-    @FXML
-    private TextField chatText;
-    @FXML
-    private Chat chatBox;
+    public TextArea chat;
+    public TextField chatText;
+    public String message = "";
+    public Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    public Chat chatBox;
 
     public void guesse(ActionEvent actionEvent) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("riddler.fxml"));
@@ -38,4 +39,30 @@ public class Controller {
         buttonBack.getScene().setRoot(root);
     }
 
+    public void sendTextInChat(ActionEvent actionEvent) {
+        if (!chatText.getText().equals("")) {
+            message += chatText.getText() + "\n";
+            chat.setText(message);
+            chatText.setText("");
+        }
+    }
+
+    public void handleEnterPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            if (!chatText.getText().equals("")) {
+                message += chatText.getText() + "\n";
+                chat.setText(message);
+                chatText.setText("");
+            }
+        }
+    }
+
+    public void openDialogPane(ActionEvent actionEvent) {
+        alert.setTitle("Подсказочка");
+        alert.setHeaderText("Это ваш IP адресс");
+        alert.setContentText("ЗДЕСЬIPАДРЕСС");
+        StringSelection ss = new StringSelection(alert.getContentText());
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+        alert.showAndWait();
+    }
 }
